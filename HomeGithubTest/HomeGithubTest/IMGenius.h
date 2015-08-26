9ab16c6205cdb88124673d25b1010df492dd22eb
byljbdh <alan@luojunbindeMacBook-Pro.local>
2015-08-26 21:26:19 +0800

    我是天才

---
+++ HomeGithubTest/HomeGithubTest/IMGenius.h
@@ -0,0 +1,13 @@
+//
+//  IMGenius.h
+//  HomeGithubTest
+//
+//  Created by 罗骏彬 on 15/8/26.
+//  Copyright (c) 2015年 蒋林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IMGenius : UIView
+
+@end
---
+++ HomeGithubTest/HomeGithubTest/IMGenius.m
@@ -0,0 +1,21 @@
+//
+//  IMGenius.m
+//  HomeGithubTest
+//
+//  Created by 罗骏彬 on 15/8/26.
+//  Copyright (c) 2015年 蒋林. All rights reserved.
+//
+
+#import "IMGenius.h"
+
+@implementation IMGenius
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
